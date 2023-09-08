426d9b238c648e1d3e6b24bce66a1ab5ce5839fd
Ronnie1501 <124007445+Ronnie1501@users.noreply.github.com>
2023-09-07 21:48:50 -0600

    to

---
+++ ActividadS6/ActividadS6/Program.cs
@@ -0,0 +1,12 @@
+﻿var numbers = new List<int> { };
+int[] scores = { 97, 92, 81, 60 };
+
+var query = from score in scores
+            where score > 80
+            select score;
+
+Console.WriteLine("Resultado:");
+foreach (var score in query)
+{
+    Console.WriteLine(score);
+}
---
+++ ActividadS62/ActividadS62/Program.cs
@@ -0,0 +1,9 @@
+﻿var numbers = new List<int> { };
+int[] scores = { 90, 71, 82, 93, 75, 82 };
+var query = scores.Where(score => score > 80).OrderByDescending(score => score);
+
+Console.WriteLine("Resultados:");
+foreach (var score in query)
+{
+    Console.WriteLine(score);
+}
